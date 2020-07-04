8b9ed486e90b7bff39ac9c615a5a6870f8ac990a
Samir Banjanovic <sbanj17@gmail.com>
2020-07-04 12:41:49 -0400

    initial commit

---
+++ src/ContentInjector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CoffeeToCode.AspNetCore.Mvc.ActionFilters
+{
+    public class ContentInjector
+        : TypeFilterAttribute
+    {
+        public ContentInjector(string parameter, string source, string path)
+            : base(typeof(ContentInjectorImpl))
+        {
+            Arguments = new object[] { parameter, source, path };
+        }
+
+        private class ContentInjectorImpl
+        {
+            private readonly string _parameter;
+            private readonly string _source;
+            private readonly string _path;
+
+            public ContentInjectorImpl(string parameter, string source, string path)
+            {
+                _parameter = parameter;
+                _source = source;
+                _path = path;
+            }
+
+            public void OnActionExecuting(ActionExecutingContext context)
+            {
+
+            }
+
+            public void OnActionExecuted(ActionExecutedContext context)
+            {
+            }
+        }
+    }
+}
